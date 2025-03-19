957c3339ef530839c437ec5675c1f8e61a1e5687
magnumfilterr <ahmadahidayat01@gmail.com>
2025-03-19 22:14:17 +0700

    fix

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,4 +1,4 @@
 class Variables {
-  static const String baseUrl = 'http://192.168.123.202:8000';
+  static const String baseUrl = 'https://smks.ujiansmks.my.id';
   // static const String baseUrl = 'http://127.0.0.1:8000';
 }
